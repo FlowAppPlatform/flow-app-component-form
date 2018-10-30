69fde8fc6f5e995f91b2273f274e5524e3fa3bf9
Mohammed Faizuddin <faiz@avantari.org>
2018-10-30 17:46:18 +0530

    changes build process to use nwb

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import React from 'react';
 import AppComponent from 'flow-app-component';
 
 class FormComponent extends AppComponent {
+  static properties = {
+    iconUrl: '/assets/images/form-component.png',
+    name: 'Form',
+    type: 'ui-component',
+    componentType: 'form',
+    category: 'Inputs',
+    parent: null,
+    showOnComponentsPanel: true,
+    isValuable: true,
+    allowsChildren: true
+  };
+
   constructor() {
     super();
     const newState = {
@@ -26,15 +38,8 @@ class FormComponent extends AppComponent {
           ],
         },
       ],
-      iconUrl: '/assets/images/form-component.png',
-      name: 'Form',
-      type: 'ui-component',
-      componentType: 'form',
-      category: 'Inputs',
-      parent: null,
-      showOnComponentsPanel: true,
-      isValuable: true,
-      allowsChildren: true,
+
+      ...FormComponent.properties
     };
 
     this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
